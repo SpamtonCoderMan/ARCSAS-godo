83e61d1cd78f548df4c81bc4eda085ce8ddc5326
Soapton <spamtoncoderman@gmail.com>
2025-05-11 14:35:21 +0100

    Main menu and simple 3d controller, kinda brocken rn

---
+++ Scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+@onready var camera_mount: Node3D = $cameraMount
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+@export var mouseSpeedH = 0.75
+@export var mouseSpeedV = 0.5
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouseSpeedH))
+		camera_mount.rotate_x(deg_to_rad(-event.relative.y * mouseSpeedV))
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "backwards")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		animation_player.play("walking")
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
